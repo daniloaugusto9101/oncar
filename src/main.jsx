cf7b58f2bff2cab4bf6db2afbd589060ea31c350
Danilo Augusto <daniloaugusto9101@gmail.com>
2024-02-07 11:43:23 -0300

    Update rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 // import App from "./App.jsx"
 import "./index.css"
 import { RouterProvider } from "react-router-dom"
-import { router } from "./routes/index.jsx"
+import { router } from "./routes"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
