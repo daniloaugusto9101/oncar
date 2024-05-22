9acae2d2fc7a2c443a0c8b145030c66c826ff515
Rafael Lucas <rafael.lucas.snt@gmail.com>
2024-05-21 23:48:55 -0300

    [feat] Implementado metodo para editar carro

--- src/services/CarsServices.jsx
+++ src/services/CarsServices.jsx
@@ -1,33 +1,39 @@
-import api from "../config/http";
+import api from '../config/http';
 
 const getCars = async () => {
-  const { data } = await api.get("/cars");
-  return data;
+    const { data } = await api.get('/cars');
+    return data;
 };
 const getCarDetails = async (id) => {
-  const { data } = await api.get(`/cars/${id}`);
-  return data;
+    const { data } = await api.get(`/cars/${id}`);
+    return data;
 };
 
 const postCar = async (car) => {
-  const { data } = await api.post(`/cars/`, car);
-  return data;
+    const { data } = await api.post(`/cars/`, car);
+    return data;
 };
 
 const deleteCar = async (idCar) => {
-  const { data } = await api.delete(`/cars/${idCar}`);
-  return data;
+    const { data } = await api.delete(`/cars/${idCar}`);
+    return data;
 };
 
 const postFinance = async (cliente) => {
-  const { data } = await api.post(`/cars/finance`, cliente);
-  return data;
+    const { data } = await api.post(`/cars/finance`, cliente);
+    return data;
+};
+
+const editCar = async (idCar, updatedCarData) => {
+    const { data } = await api.put(`/cars/${idCar}`, updatedCarData);
+    return data;
 };
 
 export default {
-  getCars,
-  getCarDetails,
-  postCar,
-  deleteCar,
-  postFinance,
+    getCars,
+    getCarDetails,
+    postCar,
+    deleteCar,
+    postFinance,
+    editCar,
 };
