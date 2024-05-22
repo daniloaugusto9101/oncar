64c29b76b0892317382b355770f2183fb9ef4916
Rafael Lucas <rafael.lucas.snt@gmail.com>
2024-05-21 23:49:32 -0300

    [feat] Adicionado rota para editar carro

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,32 +1,38 @@
-import { createBrowserRouter } from "react-router-dom";
-import RootLayout from "../layout/RootLayout";
-import Home from "../pages/Home";
-import CarDetails from "../pages/CarDetails/CarDetails";
-import NewCar from "../pages/NewCar/NewCar";
-import Finance from "../pages/Finance/Finance";
+import { createBrowserRouter } from 'react-router-dom';
+import RootLayout from '../layout/RootLayout';
+import Home from '../pages/Home';
+import CarDetails from '../pages/CarDetails/CarDetails';
+import NewCar from '../pages/NewCar/NewCar';
+import Finance from '../pages/Finance/Finance';
+import EditCar from '../pages/EditCar';
 
 export const router = createBrowserRouter([
-  {
-    path: "/oncar",
-    element: <RootLayout />,
+    {
+        path: '/oncar',
+        element: <RootLayout />,
 
-    children: [
-      {
-        path: "/oncar",
-        element: <Home />,
-      },
-      {
-        path: "/oncar/car/:carId",
-        element: <CarDetails />,
-      },
-      {
-        path: "/oncar/car/add",
-        element: <NewCar />,
-      },
-      {
-        path: "/oncar/finance",
-        element: <Finance />,
-      },
-    ],
-  },
+        children: [
+            {
+                path: '/oncar',
+                element: <Home />,
+            },
+            {
+                path: '/oncar/car/:carId',
+                element: <CarDetails />,
+            },
+            {
+                path: '/oncar/car/add',
+                element: <NewCar />,
+            },
+            {
+                path: '/oncar/car/edit/:carId',
+                element: <EditCar />,
+                props: true,
+            },
+            {
+                path: '/oncar/finance',
+                element: <Finance />,
+            },
+        ],
+    },
 ]);
